90f173757aaed9374a9cdbfa614e1d9bcae6c8bf
= <=>
2018-10-25 18:32:30 -0200

    hibernate dao

--- aula031/src/main/java/br/com/cursojava/aula031/Contato.java
+++ aula031/src/main/java/br/com/cursojava/aula031/Contato.java
@@ -8,21 +8,30 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name="pessoas")
-public class Pessoa {
+@Table(name="\"TBL_Contatos\"")
+public class Contato {
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
 	
-	@Column(nullable = false, length = 100)
+	@Column(nullable = false)
 	private String nome;
-
-	public Pessoa() {}
-
-	public Pessoa(Integer id, String nome) {
+	
+	@Column(nullable = false, unique = true)
+	private String email;
+	
+	@Column(nullable = false)
+	private String telefone;
+	
+	public Contato() {}
+	
+	public Contato(Integer id, String nome, String email, String telefone) {
+		super();
 		this.id = id;
 		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
 	}
 	public Integer getId() {
 		return id;
@@ -35,6 +44,18 @@ public class Pessoa {
 	}
 	public void setNome(String nome) {
 		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
 	}
 	@Override
 	public int hashCode() {
@@ -51,7 +72,7 @@ public class Pessoa {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Pessoa other = (Pessoa) obj;
+		Contato other = (Contato) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
@@ -59,8 +80,10 @@ public class Pessoa {
 			return false;
 		return true;
 	}
-	
-	
+	@Override
+	public String toString() {
+		return "Contato [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone + "]";
+	}
 	
 	
 
